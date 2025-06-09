4bb36c4e952ac1d2424e300dd279144c5aec7f5a
Taynara <taynaradasilva2001@gmail.com>
2025-06-09 10:59:59 -0300

    inicio calculadora

---
+++ aula09_javascript/exe1.js
@@ -0,0 +1,21 @@
+// function mostrarOla() {
+//     let nome = document.getElementById("nome").value;
+//     document.getElementById("resposta").innerHTML = "Olá, " + nome;
+// }
+
+
+const coletarNumeros = () => {
+    let num = document.getElementById("num").value;
+}
+
+const calcular = () => {
+
+}
+
+const mostrarResultado = () => {
+    let nome = document.getElementById("nome").value;
+    document.getElementById("resposta").innerHTML = "Olá, " + nome;
+}
+
+const mostrar = document.getElementById("butEnviar");
+mostrar.addEventListener("click", mostrarOla);
