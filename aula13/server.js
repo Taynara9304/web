957ccffd2a0898c3240f7574a92441f6a028256b
Taynara <taynaradasilva2001@gmail.com>
2025-08-15 09:55:53 -0300

    troca do path,ta dando erro

--- aula13/server.js
+++ aula13/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import dadosRota from './rotas/rotas.js';
-import cors from 'cors';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = Path2D.dirname(filename);
 
 const app = express();
 const PORT = process.env.PORT || 1234;
-app.use(cors());
 
 /*Middleware para interpretar o json */
 app.use(express.json());
 
 /*rota da api*/
 app.use('/', dadosRota);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log('Servidor rodando na porta 1234');
